d38b53f51b765b59fd11195fc69c974398a12fb5
glasket <cws@glasket.com>
2023-04-16 03:02:37 -0400

    Implement Card component

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,5 +1,45 @@
-type Properties = {};
+import styles from './Card.module.css';
 
-const Card = (props: Properties) => <div></div>;
+type BaseProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
 
-export { Card };
+type Omissions = keyof Pick<BaseProps, 'className' | 'style'>;
+
+type Inclusions = {
+  headerImages?: readonly [string?, string?, string?];
+  clickable?: boolean;
+};
+
+type Properties = Omit<BaseProps, Omissions> & Inclusions;
+
+export const Card = ({
+  headerImages,
+  children,
+  clickable,
+  ...props
+}: Properties) => {
+  const hasImages = headerImages !== undefined && headerImages.length > 0;
+
+  const style = `${styles['card']}${
+    clickable ? ` ${styles['clickable']}` : ''
+  }`;
+
+  return (
+    <div
+      className={style}
+      style={hasImages ? undefined : { gridTemplateRows: '1fr' }}
+      {...props}
+    >
+      {hasImages && (
+        <div className={styles['images']}>
+          {headerImages.map((i) => (
+            <img src={i} />
+          ))}
+        </div>
+      )}
+      <div className={styles['content']}>{children}</div>
+    </div>
+  );
+};
