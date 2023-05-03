7736680a65a2a609bcc9d4aa04a880fb77deaa21
glasket <cws@glasket.com>
2023-05-03 01:22:10 -0400

    Reformat run-local.sh

--- backend/run-local.sh
+++ backend/run-local.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-sam local start-api --env-vars sam-vars.json --parameter-overrides "AllowedOrigin=https://localhost:8080" --warm-containers LAZY --docker-network kga_net --profile ${1:?'Must provide AWS profile name'}
+sam local start-api \
+--env-vars sam-vars.json \
+--parameter-overrides "AllowedOrigin=https://localhost:8080" \
+--warm-containers LAZY \
+--docker-network kga_net \
+--profile ${1:?'Must provide AWS profile name'}
